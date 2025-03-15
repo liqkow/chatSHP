316e113363284ea6fb99decf390fdf3ed274527c
liqkow <mglusenko515@gmail.com>
2025-03-15 15:03:09 +1000

    update

--- pages/chats.vue
+++ pages/chats.vue
@@ -25,6 +25,7 @@
         chats: [
           { id: 1, name: "Ivan", messages: [] },
           { id: 2, name: "Anton", messages: [] },
+            { id: 3, name: "Vova", messages: [] },
         ],
         selectedChat: null,
       };
